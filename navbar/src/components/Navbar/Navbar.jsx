96d373c34d6946e68ec1cf6f047bc20c886547d0
Enrico Mani <manienrico13@gmail.com>
2023-08-27 13:38:53 +0300

    generalised navbar structure and content done

--- navbar/src/components/Navbar/Navbar.jsx
+++ navbar/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
+import './navbar.css'
+
 const Navbar=()=> {
   return (
     <header className='app__navbar'>
-        <div className="app__navbar-logo">
-            <a href="#"></a>
-        </div>
+        <a href="#" className="logo">LOGO</a>
+        <div className="menuToggle"></div>
         <nav className="app__navbar-menu">
             <ul>
                 <li><a href="#">Home</a></li>
@@ -11,7 +12,13 @@ const Navbar=()=> {
                 <li><a href="#">Dropdown</a>
                   <ul>
                     <li><a href="#">Product 01</a></li>
-                    <li><a href="#">Product 02</a></li>
+                    <li><a href="#">Product 02<b>+</b></a>
+                      <ul>
+                        <li><a href='#'>Submenu 01</a></li>
+                        <li><a href='#'>Submenu 02</a></li>
+                        <li><a href='#'>Submenu 03</a></li>
+                      </ul>
+                    </li>
                   </ul>
                 </li>
                 <li><a href="#">Portfolio</a></li>
@@ -19,7 +26,7 @@ const Navbar=()=> {
                   <ul>
                     <li><a href="#">Post Link 01</a></li>
                     <li><a href="#">Post Link 02</a></li>
-                    <li><a href="#">Post Link 03</a></li>
+                    <li><a href="#">Post Link 03<b>+</b></a></li>
                   </ul>
                 </li>
                 <li><a href="#">Contact</a></li>
